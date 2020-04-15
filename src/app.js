bece48180376c030a9e2933ec15ba06c07c68abc
Claudeilton Dantas <fcd007@hotmail.com>
2020-04-15 20:29:32 -0300

    Project to test GoStack Sucess Run

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -31,11 +31,41 @@ app.post("/repositories", (request, response) => {
 });
 
 app.put("/repositories/:id", (request, response) => {
-  //
+  // title, url, techs do id contido na rota
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+  
+  const repositoryIndex = repositories.findIndex( repository => repository.id === id );
+
+  if(!isUuid(id)){
+    return response.status(400).json({ error: 'Bad Request'});
+  }
+
+  const repository = {
+    id,
+    url,
+    title,
+    techs,
+    likes: 0,
+  };
+
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  //
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if(!isUuid(id)) {
+    return response.status(400).json({error: 'Bad Request'});
+  }
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
@@ -43,11 +73,11 @@ app.post("/repositories/:id/like", (request, response) => {
 
   const repository = repositories.find(repository => repository.id === id);
   
-  if(repository < 0){
-    return response.status(400).json( { error: 'repository not found.'});
+  if(!repository) {
+    return response.status(400).json( { error: 'Bad Request'});
   }
 
-  repository.likes += 1;
+  repository.likes++;
   
   return response.json(repository);
 });
